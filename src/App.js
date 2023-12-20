a82a3583be04f5eb6cfb82131b7117abdf7e46d5
yagmurnisa <ymr.nisa@hotmail.com>
2023-12-20 10:42:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useState, useRef } from 'react';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  const [showLink, setShowLink] = useState(false);
+  const [showSearch, setshowSearch] = useState(false);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (search.trim().length !== 0 ) {
+      navigate(`/search?q=${search}`);
+    }
+  };
+  return (
+    <Fragment>
+    <nav>
+        <div className='logo'>BLOG</div>
+        <button className='searchbtn' onClick={()=> {setshowSearch(!showSearch); setShowLink(false)}}>{showSearch ? <FontAwesomeIcon icon={faTimes} color="white"/> : <FontAwesomeIcon icon={faSearch} color="white"/>}</button>
+        <form className={showSearch ? "open": ""} onSubmit={(e)=> handleSearch(e)}>
+            <input type='text' placeholder='Search' value={search} onChange={(e)=> {setSearch(e.target.value)}}/>
+            <button type='submit'><FontAwesomeIcon icon={faSearch} color="#141414"/></button>
+        </form>
+        <button className='togglebtn' onClick={()=> {setShowLink(!showLink); setshowSearch(false)}}>
+        {showLink ? <FontAwesomeIcon icon={faTimes} color="white"/> : <FontAwesomeIcon icon={faBars} color="white"/>}
+        </button>
+        <ul className={showLink ? "open": ""}>
+            <li>
+                <NavLink className='navLink' to='/login'>Login</NavLink>
+            </li>
+            <li>
+                <NavLink className='navLink' to='/register'>Register</NavLink>
+            </li>
+            <li>
+                <NavLink className='navLink' to='/about'>About</NavLink>
+            </li>   
+            <li>
+                <NavLink className='navLink' to='/posts'>Posts</NavLink>
+            </li>
+        </ul>
+    </nav>
+    </Fragment>
+  )
+}
+export default Navbar;
